c44d1236912e076a6c51812292aeb2b178b284c9
전지훈 <junji636@gmail.com>
2022-12-17 23:56:19 +0900

    firstCommit

---
+++ SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapBottomSheetApp.swift
+//  SwiftUI_MapBottomSheet
+//
+//  Created by 전지훈 on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
