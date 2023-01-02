0be3dae8ad5435df65d2070c5b4881800bf67c03
전지훈 <junji636@gmail.com>
2023-01-02 23:57:10 +0900

    ignoreSafeArea추가

--- SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Home.swift
+++ SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Home.swift
@@ -11,6 +11,7 @@ struct Home: View {
             //MARK: Sample Coordinate Region
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 25.2048, longitude: 55.2708), latitudinalMeters: 10000, longitudinalMeters: 10000)
             Map(coordinateRegion: .constant(region))
+                .ignoresSafeArea()
         }
     }
 }
