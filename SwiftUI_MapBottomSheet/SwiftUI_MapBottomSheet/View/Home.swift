c1a1032e433268472ff5ff027b68c2a138d7c899
전지훈 <junji636@gmail.com>
2023-01-03 23:52:33 +0900

    sheet추가 작업중

--- SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Home.swift
+++ SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Home.swift
@@ -12,6 +12,20 @@ struct Home: View {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 25.2048, longitude: 55.2708), latitudinalMeters: 10000, longitudinalMeters: 10000)
             Map(coordinateRegion: .constant(region))
                 .ignoresSafeArea()
+            //MARK: Building Sheet UI
+                .sheet(isPresented: .constant(true)) {
+                    VStack(spacing: 15) {
+                        TextField("Search Maps", text: .constant(""))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background {
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.ultraThickMaterial)
+                        }
+                    }
+                    .padding()
+                    .padding(.top)
+                }
         }
     }
 }
