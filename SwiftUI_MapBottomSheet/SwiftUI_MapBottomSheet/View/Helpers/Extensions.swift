1f1238c74bfab75a0041f6be7682a1b0fffeb556
전지훈 <junji636@gmail.com>
2023-01-07 23:57:50 +0900

    extension추가중

--- SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Helpers/Extensions.swift
+++ SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Helpers/Extensions.swift
@@ -5,5 +5,24 @@
 import SwiftUI
 
 extension View {
-    
+    @ViewBuilder
+    func bottomSheet<Content: View> (
+        presentationDetents: Set<PresentationDetent>,
+        isPresented: Binding<Bool>,
+        dragIndicator: Visibility = .visible,
+        sheetCornerRadius: CGFloat?,
+        largestundimmedIdentifier: UISheetPresentationController.Detent.Identifier = .large,
+        isTransprentBG: Bool = false,
+        interactiveDisabled: Bool = true,
+        @ViewBuilder content: @escaping ()->Content,
+        onDismiss: @escaping ()->()
+    ) -> some View {
+        self.sheet(isPresented: isPresented) {
+            onDismiss()
+        } content: {
+            content()
+                .presentationDetents(presentationDetents)
+                .presentationDragIndicator(dragIndicator)
+        }
+    }
 }
