21f520850a9326ea91fda636e3ca40ac034a6da7
전지훈 <junji636@gmail.com>
2023-01-11 23:58:26 +0900

    설정버튼 추가

--- SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Home.swift
+++ SwiftUI_MapBottomSheet/SwiftUI_MapBottomSheet/View/Home.swift
@@ -12,6 +12,15 @@ struct Home: View {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 25.2048, longitude: 55.2708), latitudinalMeters: 10000, longitudinalMeters: 10000)
             Map(coordinateRegion: .constant(region))
                 .ignoresSafeArea()
+                .overlay(alignment: .topTrailing, content: {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "gearshape.fill")
+                            .font(.title2)
+                    }
+                    .padding()
+                })
             //MARK: Building Sheet UI
                 .bottomSheet(presentationDetents: [.medium, .large, .height(70)], isPresented: .constant(true), sheetCornerRadius: 20) {
                     ScrollView(.vertical, showsIndicators: false) {
